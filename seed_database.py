6b61231a813f7a98ffe87e47098e798b07613989
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-05-10 12:52:03 -0400

    Receive JSON data

--- seed_database.py
+++ seed_database.py
@@ -14,3 +14,17 @@ os.system('createdb booksdb')
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+with open('data/books.json') as f:
+    book_data = json.loads(f.read())
+    print(book_data)
+
+# Create books, store them in list so we can use them
+# to create fake ratings later
+books_in_db = []
+for book in book_data:
+    # TODO: get the title, overview, and poster_path from the movie
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+
+    # TODO: create a movie here and append it to movies_in_db
