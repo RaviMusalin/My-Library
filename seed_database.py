6e83d1d73be5c9f01d14fb182fe534c7b8c2143d
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-05-10 14:23:10 -0400

    Seed Database: Create Book complete

--- seed_database.py
+++ seed_database.py
@@ -23,7 +23,16 @@ with open('data/books.json') as f:
 # to create fake ratings later
 books_in_db = []
 for book in book_data:
-    # TODO: get the title, author, and genre from the book
+    title, author, genre = (
+        book["title"],
+        book["author"],
+        book["genre"],
+    )
     # dictionary. Then, Get the ISBN and book cover (From API?)
 
-    # TODO: create a movie here and append it to books_in_db
+    # TODO: create a book here and append it to books_in_db
+    db_book = crud.create_book(title, author, genre)
+    books_in_db.append(db_book)
+
+model.db.session.add_all(books_in_db)
+model.db.session.commit()
