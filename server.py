b8fd2332d63ef7c3153f2ba33f117ec9863f9604
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-05-11 15:24:50 -0400

    Flask set up

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 """Server for library  app."""
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db, db
+import crud
 
-from flask import Flask
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 # Replace this with routes and view functions!
 
 
 if __name__ == "__main__":
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
